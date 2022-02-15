148bc109f90fd99e7bf79e07d80cbb6173462aa2
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-15 13:52:13 +0700

    add middleware bodyparser and notesRouter

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express()
 const port = process.env.PORT
 const host = process.env.HOST
 
+const bodyParser = require('body-parser');
+const notesRoute = require('./src/notes/routes') 
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+app.use(notesRoute);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,3 +23,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on http://${host}:${port}`)
 })
+
