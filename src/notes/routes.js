ade6c7d326fafd478d40bf9cb764b0ab6e832f40
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-15 15:36:07 +0700

    Create route POST: /notes

--- src/notes/routes.js
+++ src/notes/routes.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const route = express.Router();
+const NoteServices = require('./note.services') 
 
-route.post('/notes', async (req, res) => {
+route.post('/notes', async(req, res) => {
 
+    const {title, body} = req.body;
+
+    await new NoteServices().addNote({title, body})
     
+    res.json('Create a new note successfully')
 
 });
 
-route.get('/notes', (req, res) => {
+// route.get('/notes', (req, res) => {
 
-});
+// });
 
-route.get('/notes/:notesID', (req, res) => {
+// route.get('/notes/:notesID', (req, res) => {
 
-});
+// });
+
+module.exports = route;
