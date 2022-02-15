f2458478097e6f06ed92ad14274ad81b45e8af05
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-15 13:52:48 +0700

    Create handling request post /notes

---
+++ src/notes/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const route = express.Router();
+
+route.post('/notes', async (req, res) => {
+
+    
+
+});
+
+route.get('/notes', (req, res) => {
+
+});
+
+route.get('/notes/:notesID', (req, res) => {
+
+});
