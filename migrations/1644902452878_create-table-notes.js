68abcbe45313206cbdaecac2dd0fe64012001091
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-15 12:25:10 +0700

    Add code create table notes and add  struktur of table

--- migrations/1644902452878_create-table-notes.js
+++ migrations/1644902452878_create-table-notes.js
@@ -1,9 +1,32 @@
 /* eslint-disable camelcase */
 
+const { PgType } = require("node-pg-migrate");
+
 exports.shorthands = undefined;
 
 exports.up = pgm => {
-    pgm.createTable()
+    pgm.createTable('notes', {
+        id: {
+            type: 'VARCHAR(50)',
+            notNull: true
+        },
+        title: {
+            type: 'TEXT',
+            notNull: true
+        },
+        body: {
+            type: 'TEXT',
+            notNull: true
+        },
+        createAt: {
+            type: 'TEXT',
+            notNull: true
+        },
+        createAt: {
+            type: 'TEXT',
+            notNull: true
+        },
+    })
 };
 
 exports.down = pgm => {};
