d19bc161b1590164a18f11eb72d5923ff2466dcb
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-15 15:35:50 +0700

    Add new note.services.js

---
+++ src/notes/note.services.js
@@ -0,0 +1,33 @@
+const { nanoid } = require("nanoid");
+const { Pool } = require("pg");
+
+class NoteServices {
+    
+    constructor() {
+        this._poll = new Pool();
+    }
+
+    async addNote({title, body}) {
+        
+        const id = `note-${nanoid(16)}`;
+        const createdAt = new Date().toISOString();
+        const updatedAt = createdAt;
+
+        const query = {
+            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5) RETURNING id',
+            values: [id, title, body, createdAt, updatedAt]
+        }
+
+        const result = await this._poll.query(query);
+
+        if (!result.rows[0].id) {
+            throw new Error('Add new note failure')
+        }
+
+        return result.rows[0];
+
+    }
+
+}
+
+module.exports = NoteServices;
