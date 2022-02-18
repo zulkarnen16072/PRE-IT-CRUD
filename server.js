353f120ee8148c53aef2b8830134540a3caf325b
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-19 05:56:34 +0700

    Using multer for send file data access in POST /uploads

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const host = process.env.HOST
 const bodyParser = require('body-parser');
 const notesRoute = require('./src/notes/routes'); 
 
-
+const multer = require('multer');
+const { json } = require('express/lib/response');
 
 app.use(bodyParser.json());
 
@@ -18,6 +19,58 @@ app.use(
 )
 
 app.use(notesRoute);
+
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname )
+  }
+})
+
+
+const fileFilter = (req, file, cb) => {
+  const condition = ['image/jpg', 'image/png', 'image/jpeg']
+  
+  if (condition.includes(file.mimeType)) {
+    cb(null, true)
+  }
+
+  cb(null, false) 
+  return cb(new MediaError('file tidak sesuai'))
+  
+}
+
+
+app.use(multer({
+  storage: fileStorage,
+  fileFilter
+}).single('image'));
+
+
+app.post('/uploads', (req, res) => {
+  
+  const file = req.file;
+
+  if (!file) {
+    res.status(400).json({
+      errro: {
+        code: 400,
+        status: 'bad request',
+        message: 'file required'
+      } 
+    })
+  }
+
+  res.json({
+    status: 'successfully',
+    data: req.file
+  }) 
+
+}) 
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
