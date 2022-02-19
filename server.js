647c3b0279706711a720a21d6e9226963e519b69
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-19 09:06:32 +0700

    create function responseJSON()

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const host = process.env.HOST
 const bodyParser = require('body-parser');
 const notesRoute = require('./src/notes/routes'); 
 
+const fileUpload = require('express-fileupload')
+
 const multer = require('multer');
-const { json } = require('express/lib/response');
+
+app.use(fileUpload())
 
 app.use(bodyParser.json());
 
@@ -31,6 +34,8 @@ const fileStorage = multer.diskStorage({
 })
 
 
+
+
 const fileFilter = (req, file, cb) => {
   const condition = ['image/jpg', 'image/png', 'image/jpeg']
   
@@ -39,37 +44,90 @@ const fileFilter = (req, file, cb) => {
   }
 
   cb(null, false) 
-  return cb(new MediaError('file tidak sesuai'))
+  return cb(new TypeError('extension file salah bro'))
   
 }
 
 
-app.use(multer({
-  storage: fileStorage,
-  fileFilter
-}).single('image'));
 
 
-app.post('/uploads', (req, res) => {
-  
-  const file = req.file;
 
-  if (!file) {
-    res.status(400).json({
-      errro: {
-        code: 400,
-        status: 'bad request',
-        message: 'file required'
-      } 
-    })
-  }
+// const upload = multer({
+//   storage: fileStorage,
+//   fileFilter
+// }).single('image')
 
+
+// try {
+//   app.use(upload)
+// } catch (e) {
+//   console.info(e)
+// }
+
+
+
+const resSuccess = (req, res) => {
   res.json({
     status: 'successfully',
     data: req.file
   }) 
+} 
 
-}) 
+
+const resError = (res, err) => {
+  res.status(400).json({
+    error: {
+      code: 400,
+      status: 'bad request',
+      message: err.message
+    } 
+  })
+}
+
+
+
+function responseJSON(message, response, code=400, status='bad request')  {
+
+  return response
+  .status(400)
+  .json({
+    error: {
+      code,
+      status: 'bad request',
+      message,
+    }
+  })
+
+  //response.end()
+
+}
+
+
+
+app.post('/uploads/test', (req, res) => {
+  
+  console.info(req.files)
+
+  if (!req.files) {
+    responseJSON('file requires', res);
+    return;
+  }
+
+  // if (req.files) {
+  //   console.info('hi')
+  // } else {
+  //   console.info('hmm');
+  // }
+  
+  
+    
+
+
+ 
+  
+})
+
+  
 
 
 app.get('/', (req, res) => {
