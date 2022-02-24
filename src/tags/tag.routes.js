b9b81f3f58e08c025506157fa09e98f68d7e2596
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-24 23:02:43 +0700

    add POST /tags

--- src/tags/tag.routes.js
+++ src/tags/tag.routes.js
@@ -21,5 +21,25 @@ routeTags.get('/tags', async (req, res) => {
 })
 
 
+
+
+routeTags.post('/tags', async (req, res) => {
+
+    try {
+        const tag_id = await (await tagServices.addTags(req.body));
+
+        res.json({
+            code: 200,
+            success: true,
+            message: 'created one tags successfully',
+            tag_id
+
+        })
+    } catch(e) {
+        console.log(e.stack)
+    }
+
+})
+
 module.exports = routeTags;
 
