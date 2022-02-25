7ffae4fc6555bff603a954b1e539e28316f9ffa9
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-25 07:41:56 +0700

    update POST /tags/{id}

--- src/tags/tag.routes.js
+++ src/tags/tag.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const NoteServices = require('../notes/note.services');
 const TagsServices = require('./tag.services');
 const routeTags = express.Router();
 
 const tagServices = new TagsServices()
+const noteServices = new NoteServices()
+
 
 routeTags.get('/tags', async (req, res) => {
 
@@ -21,6 +24,50 @@ routeTags.get('/tags', async (req, res) => {
 })
 
 
+routeTags.get('/tags/:id', async (req, res) => {
+
+    const tagID = req.params.id;
+
+    try {
+        const tags =  await tagServices.getTagById(tagID);
+        const notes = await noteServices.getNoteByTagID(tagID);
+        const countNotes = notes.rowCount;
+        const noteRows = notes.rows.map(e => 
+            {
+                return {
+                    id: e.id,
+                    title: e.title
+                }
+            })
+
+
+        console.info('Tags: ', tags)
+
+        console.info('notes: ', notes)
+
+        const data = {
+            id: tags.tag_id,
+            name: tags.name,
+            countNotes,
+            notes: noteRows
+            
+        }
+
+
+        console.info(data)
+
+        res.json({
+            success: true,
+            code: 200,
+            data
+            
+
+        })
+    } catch(e) {
+        console.info(e)
+    }
+
+})
 
 
 routeTags.post('/tags', async (req, res) => {
@@ -40,6 +87,11 @@ routeTags.post('/tags', async (req, res) => {
     }
 
 })
+
+
+
+
+
 
 module.exports = routeTags;
 
